8ded2963f9a98e0edf47492866d7da00c4aacaf7
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-12 16:56:14 -0300

    feat: Creates and mplements get_all_orders_from_customer method

--- src/track_orders.py
+++ src/track_orders.py
@@ -10,6 +10,16 @@ class TrackOrders:
         new_order = (customer, order, day)
         self.orders.append(new_order)
 
+    def get_all_orders_from_customer(self, customer):
+        all_orders_from_customer = dict()
+        for order in self.orders:
+            if order[0] == customer:
+                if order[1] not in all_orders_from_customer:
+                    all_orders_from_customer[order[1]] = 1
+                else:
+                    all_orders_from_customer[order[1]] += 1
+
+        return all_orders_from_customer
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
