603e0b68f37f247ba8e6b3bc219a4092b0f9608b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-12 14:50:33 -0300

    feat: Implements csv_reader function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,18 @@
+import csv
+
+
+def csv_reader(path):
+    if not path.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path}'")
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file, delimiter=',', quotechar='"')
+
+            data = [*csv_reader]
+
+            return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path}'")
+
 def analyze_log(path_to_file):
     raise NotImplementedError
